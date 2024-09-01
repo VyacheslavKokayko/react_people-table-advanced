3c5c6be4a36c5ebc5a5247b797c7e59c1ac9a692
Vyacheslav Kokayko <vkokayko@gmail.com>
2024-09-01 07:36:31 +0200

    REACT_PEOPLE_TABLE_ADVANCED

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import { PeoplePage } from './components/PeoplePage';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
+import { PeoplePage } from './components/Page/PeoplePage';
+import { NotFoundPage } from './components/Page/NotFoundPage';
+import { HomePage } from './components/Page/HomePage';
 
 import './App.scss';
+import React = require('react');
 
 export const App = () => {
   return (
@@ -10,9 +14,15 @@ export const App = () => {
 
       <div className="section">
         <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="home" element={<Navigate to="/" replace />} />
+            <Route path="people">
+              <Route index element={<PeoplePage />} />
+              <Route path=":slug" element={<PeoplePage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/components/Page/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import React = require('react');
+
+export const NotFoundPage = () => {
+  return (
+    <div data-cy="app">
+      <h1 className="title">Page not found</h1>
+    </div>
+  );
+};
